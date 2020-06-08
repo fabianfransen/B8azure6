1e9977dd15b0dd3561b30186e10516d6e3b0ad3d
sannepost2001 <43266412+sannepost2001@users.noreply.github.com>
2020-06-08 19:32:25 +0200

    Add files via upload

--- application.py
+++ application.py
@@ -20,16 +20,16 @@ def page():
     # searchword = request.form["myTextarea"]
     startdate = request.form["startdate"]
     enddate = request.form["enddate"]
-
     gene = request.form["gene"]
     # print(searchword)
     zoekwoord = request.form["myTextarea"]
-    count, zoekterm = search_count(zoekwoord, startdate, gene)
+    count, zoekterm = search_count(zoekwoord, startdate, gene, enddate)
     id_list = search_artikel(zoekterm, count)
     hgnc_genen, dict_genpanels = genpanel()
     resultaat = gegevens(id_list, hgnc_genen, dict_genpanels)
-    print(resultaat)
-    teruggave = ("   <table id=\"ResultTable\" style=\"width:777px; height: 400px; border-collapse: collapse; padding: 10px;\""
+
+    teruggave = ("   <table id=\"ResultTable\" style=\"width:777px; height: 400px; border-collapse: collapse; "
+                 "padding: 10px;\""
                  " class=\"sortable-table\" border=\"1\" border-collapse=\"collapse\">"
                  + "   <thead>\n"
                  + "   <tr>\n"
@@ -37,14 +37,15 @@ def page():
                  + "   <th style=\"padding: 10px;\"><p1>Title</p1></th>\n"
                  + "   <th style=\"padding: 10px;\" class=\"date-sort\"><p1>Date publication</p1></th>\n"
                  + "   <th style=\"padding: 10px;\" class=\"date-sort\"><p1>Date last revised</p1></th>\n"
-                 + "   <th style=\"padding: 10px;\"><p1>Gene</p1></th>\n"
+                 + "   <th style=\"padding: 10px;\"><p1><b>Gene</b> (genpanel)</p1></th>\n"
                  + "   </tr>"
                  + "   </thead>")
     for a in resultaat:
         teruggave = teruggave + "<tr>"
         teruggave = teruggave + "<td style=\"padding: 10px;\">" \
                                 "<a href = \"https://pubmed.ncbi.nlm.nih.gov/" + str(a[0]) + "\" target=\"_blank\">" \
-                                "" + str(a[0]) + "</a></td>"
+                                                                                             "" + str(
+            a[0]) + "</a></td>"
         teruggave = teruggave + "<td style=\"padding: 10px;\"><p2>" + str(a[1]) + "</p2></td>"
         teruggave = teruggave + "<td style=\"padding: 10px;\"><p2>" + str(a[2]) + "</p2></td>"
         teruggave = teruggave + "<td style=\"padding: 10px;\"><p2>" + str(a[3]) + "</p2></td>"
@@ -54,21 +55,37 @@ def page():
     teruggave = teruggave + "</table>"
     teruggave = teruggave + "<tr> <td colspan=4> <hr> <a id=\"downloadLink\" onclick=\"exportToExcel(this)\" style=\"cursor" \
                             ": pointer;\"> Download as excel: <img src=\"../static/images/exelimage.jpeg\" onclick=\"exportToExcel(this)\" " \
-                            "title=\"Exporteer naar Excel.\"> </a> <br> "\
+                            "title=\"Exporteer naar Excel.\"> </a> <br> " \
                             "<a id=\"downloadLink\" onclick=\"exportToCSV(this)\" style=\"cursor" \
                             ": pointer;\">Download as CSV: <img src=\"../static/images/CSV-icon.png\" onclick=\"exportToCSV(this)\" " \
                             "title=\"Exporteer naar CSV.\" width=\"16\"> </a> </td> </tr>"
 
-    return render_template("page.html", zoekwoord=zoekwoord, teruggave=teruggave, startdate=startdate, gene=gene)
+    if startdate != "" and enddate != "" and gene != "":
+        zoekwoord = zoekwoord + ", start date: " + startdate + ", end date: " + enddate + ", gene: " + gene
+    elif startdate == "" and enddate != "" and gene != "":
+        zoekwoord = zoekwoord + ", " + enddate + ", gene: " + gene
+    elif startdate != "" and enddate == "" and gene != "":
+        zoekwoord = zoekwoord + ", start date: " + startdate + ", gene: " + gene
+    elif startdate != "" and enddate != "" and gene == "":
+        zoekwoord = zoekwoord + ", start date: " + startdate + ", end date: " + enddate
+
+    return render_template("page.html", zoekwoord=zoekwoord, teruggave=teruggave, startdate=startdate,
+                           enddate=enddate, gene=gene)
 
 
-def search_count(zoekwoord, startdate, gene):
+def search_count(zoekwoord, startdate, gene, enddate):
     x = datetime.datetime.now()
     jaar = x.strftime("%Y")
-    if startdate == "":
-        zoekterm = zoekwoord + " AND 1950:{} [dp]".format(jaar)
+    maand = x.strftime("%m")
+    jaar_5 = int(x.strftime("%Y")) - 5
+    if startdate == "" and enddate != "":
+        zoekterm = zoekwoord + " AND {}:{} [dp]".format(jaar_5, enddate)
+    elif enddate == "" and startdate != "":
+        zoekterm = zoekwoord + " AND {}:{}/{} [dp]".format(startdate, jaar, maand)
+    elif startdate == "" and enddate == "":
+        zoekterm = zoekwoord + " AND {}:{} [dp]".format(jaar_5, jaar)
     else:
-        zoekterm = zoekwoord + " AND {}:{} [dp]".format(startdate, jaar)
+        zoekterm = zoekwoord + " AND {}:{} [dp]".format(startdate, enddate)
     ingevulde_genen = str(gene).split(" ")
     if len(gene) != 0:
         for gen in ingevulde_genen:
@@ -93,31 +110,33 @@ def search_artikel(zoekterm, count):
 
 def gegevens(id_list, hgnc_genen, dict_genpanels):
     resultaat = []
-    for i in range(len(id_list)):
-        handle = Entrez.efetch(db="pubmed", id=id_list[i], rettype="medline")
-        records = Medline.parse(handle)
-        for record in records:
-            resultaatperhit = []
-            gen = ""
-            resultaatperhit.append(record['PMID'])
-            resultaatperhit.append(record['TI'])
-            datum_nieuw = dag(str(record['DP']))
-            datum_compleet = datum(datum_nieuw)
-            resultaatperhit.append(datum_compleet)
-            datum_nieuw = datum_maken(str(record['LR']))
-            datum_compleet = datum(datum_nieuw)
-            resultaatperhit.append(datum_compleet)
-            abstract = record['AB']
-            gevonden_genen, abstract = genen_genpanel(abstract, hgnc_genen)
-            gevonden_genen = genen(gevonden_genen, abstract)
-            gevonden_genpanels = aanwezige_genpanels(gevonden_genen, dict_genpanels)
-            hgnc_gevonden_genen = gen_namen(gevonden_genen)
-            for item in hgnc_gevonden_genen:
-                gen = gen + item + " "
-            resultaatperhit.append(gen)
-            resultaatperhit.append(gevonden_genpanels)
-            resultaat.append(resultaatperhit)
+    try:
+        for i in range(len(id_list)):
+            handle = Entrez.efetch(db="pubmed", id=id_list[i], rettype="medline")
+            records = Medline.parse(handle)
+            for record in records:
+                resultaatperhit = []
+                gen = ""
+                resultaatperhit.append(record['PMID'])
+                resultaatperhit.append(record['TI'])
+                datum_nieuw = dag(str(record['DP']))
+                datum_compleet = datum(datum_nieuw)
+                resultaatperhit.append(datum_compleet)
+                datum_nieuw = datum_maken(str(record['LR']))
+                datum_compleet = datum(datum_nieuw)
+                resultaatperhit.append(datum_compleet)
+                abstract = record['AB']
+                gevonden_genen, abstract = genen_genpanel(abstract, hgnc_genen)
+                gevonden_genen = genen(gevonden_genen, abstract)
+                hgnc_gevonden_genen = gen_namen(gevonden_genen)
+                gevonden_genpanels = aanwezige_genpanels(hgnc_gevonden_genen, dict_genpanels)
+                resultaatperhit.append(gevonden_genpanels)
+                resultaat.append(resultaatperhit)
+    except KeyError:
+        pass
+
     return resultaat
+
 
 def gen_namen(gevonden_genen):
     try:
@@ -136,15 +155,20 @@ def gen_namen(gevonden_genen):
         response, content = h.request(target.geturl(), method, body, headers)
         if response['status'] == '200':
             data = json.loads(content)
-            print('Symbol:' + data['response']['docs'][0]['symbol'])
-            hgnc_gevonden_genen.append(data['response']['docs'][0]['symbol'])
+            try:
+                # print('Symbol:' + data['response']['docs'][0]['symbol'])
+                hgnc_gevonden_genen.append(data['response']['docs'][0]['symbol'])
+            except IndexError:
+                print("Gene removed")
         else:
             print('Error detected: ' + response['status'])
     return hgnc_gevonden_genen
 
+
 def genpanel():
     hgnc_genen = []
-    bestand = open("GenPanels_merged_DG-2.17.0.txt", 'r')
+    bestand = open(
+        "GenPanels_merged_DG-2.17.0.txt", 'r')
     dict_genpanels = {}
     bestand.readline()
     for line in bestand:
@@ -210,7 +234,8 @@ def datum_maken(datum_):
 
 def dag(date):
     try:
-        dicht_dagen = {"1": "01", "2": "02", "3": "03", "4": "04", "5": "05", "6": "06", "7": "07", "8": "08", "9": "09"}
+        dicht_dagen = {"1": "01", "2": "02", "3": "03", "4": "04", "5": "05", "6": "06", "7": "07", "8": "08",
+                       "9": "09"}
         dagen = date.split(" ")
         dag_ = dicht_dagen.get(dagen[2])
         if dag_ is None:
@@ -246,12 +271,14 @@ def datum(date):
     return datum_compleet
 
 
-
 def aanwezige_genpanels(gevonden_genen, dict_genpanels):
     gevonden_genpanels = ""
     for gen in gevonden_genen:
         if dict_genpanels.get(gen) is not None:
-            gevonden_genpanels = gevonden_genpanels + dict_genpanels.get(gen) + "\n"
-
+            gevonden_genpanels = gevonden_genpanels + "<b>" + gen + "</b>" + " (" + dict_genpanels.get(gen) + ")  " \
+                                 + "<br>"
+        else:
+            gevonden_genpanels = gevonden_genpanels + "<b>" + gen + "</b>" + "  " + "<br>"
     gevonden_genpanels = gevonden_genpanels.replace("\n", "")
+
     return gevonden_genpanels
